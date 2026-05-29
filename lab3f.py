3485a0f8d0e047ac53165be753dfaba9404bbbc2
Sujana Poudel <spoudel26@myseneca.ca>
2026-05-29 12:41:56 -0400

    fix lab3f functions

--- lab3f.py
+++ lab3f.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-my_list = [1, 2, 3, 4]
+my_list = [1, 2, 3, 4, 5, 6]
+
 
 def add_item_to_list(list_name):
+    list_name.append(7)
+    list_name.append(8)
+    return list_name
 
-    last_item = list_name[-1]
-
-    list_name.append(last_item + 1)
-    list_name.append(last_item + 2)
 
 def remove_items_from_list(list_name, items):
-
     for item in items:
         if item in list_name:
             list_name.remove(item)
+    return list_name
