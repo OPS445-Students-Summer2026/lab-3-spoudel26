b749bb1dde827722ca2f67a3c1f6ea5f6f8b9e2b
Sujana Poudel <spoudel26@myseneca.ca>
2026-05-29 13:04:25 -0400

    final fix lab3f

--- lab3f.py
+++ lab3f.py
@@ -2,14 +2,22 @@
 
 my_list = [1, 2, 3, 4]
 
-
 def add_item_to_list(list_name):
-    last_item = list_name[-1]
-    list_name.append(last_item + 1)
-    list_name.append(last_item + 2)
 
+    if max(list_name) < 8:
+        list_name.append(max(list_name) + 1)
+        list_name.append(max(list_name) + 1)
 
 def remove_items_from_list(list_name, items):
+
     for item in items:
-        while item in list_name:
+        if item in list_name:
             list_name.remove(item)
+
+
+if __name__ == '__main__':
+    add_item_to_list(my_list)
+    print(my_list)
+
+    remove_items_from_list(my_list, [1, 2])
+    print(my_list)
